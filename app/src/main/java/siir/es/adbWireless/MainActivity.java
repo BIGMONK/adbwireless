d4e88efdd28b098629f2e76f0b5ddc34f7947d3e
duanjunfeng <488698112@qq.com>
2021-10-26 16:52:44 +0800

    AndroidX

--- app/src/main/java/siir/es/adbWireless/MainActivity.java
+++ app/src/main/java/siir/es/adbWireless/MainActivity.java
@@ -1,6 +1,6 @@
 package siir.es.adbWireless;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
